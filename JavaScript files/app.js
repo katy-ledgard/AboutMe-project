60aaf262cf521741ea58a606c2cddb44723dbba4
Katy <katy.ledgard@gmail.com>
2023-06-13 16:58:38 +0100

    I have added 3 js questions

--- JavaScript files/app.js
+++ JavaScript files/app.js
@@ -3,8 +3,6 @@
 console.log("Welcome!")
 
 let user;
-let answer1;
-let answer2;
 
 
 function userName() {
@@ -18,20 +16,20 @@ userName()
 
 function firstQuestion() {
 
-    answer1 = prompt("Is my code working?").toLowerCase();
+    let born = prompt("Was Katy born in Norfolk?").toLowerCase();
 
-    while (answer1 != "y" && answer1 != "yes" && answer1 != "n" && answer1 != "no") {
+    while (born != "y" && born != "yes" && born != "n" && born != "no") {
             alert("Please answer with yes/no or y/n.");
-            answer1 = prompt("Is my code working?").toLowerCase();
+            born = prompt("Was Katy born in Norfolk?").toLowerCase();
     
 }
-    if (answer1 === "yes" || answer1 === "y") {
-        // console.log("You chose yes!");
-        alert("You chose yes!");
-    } else if (answer1 === "no" || answer1 === "n") {
-        // console.log("you chose no!")
-        alert("You chose no!");
-    }
+    if (born === "yes" || born === "y") {
+        // console.log("That's right! Katy was born in Norfolk!");
+        alert("That's right! Katy was born in Norfolk!");
+    } else if (born === "no" || born === "n") {
+        // console.log("Oops! Katy WAS born in Norfolk!")
+        alert("Oops! Katy WAS born in Norfolk!");
+    } 
     
 }
 
@@ -39,20 +37,40 @@ firstQuestion()
 
 function secondQuestion() {
 
-    answer2 = prompt("Is this question two?").toLowerCase();
+    let beach = prompt("Is Cromer Katy's favourite beach?").toLowerCase();
 
-    while (answer2 != "yes" && answer2 != "y" && answer2 != "no" && answer2 != "no") {
+    while (beach != "yes" && beach != "y" && beach != "no" && beach != "n") {
         alert("Please answer with yes/no or y/n.")
-        answer2 = prompt("Is this question two?").toLowerCase();
+        beach = prompt("Is Cromer Katy's favourite beach?").toLowerCase();
     }
 
-    if (answer2 === "yes" || answer2 === "y") {
-        // console.log("You chose yes!");
-        alert("You chose yes!");        
-    } else if (answer2 === "no" || answer2 === "n") {
-        // console.log("You chose no!");
-        alert("You chose no!");        
-    }
+    if (beach === "yes" || beach === "y") {
+        // console.log("Not quite! Katy's favourite beach is West Runton!");
+        alert("Not quite! Katy's favourite beach is West Runton!");        
+    } else if (beach === "no" || beach === "n") {
+        // console.log("That's right! Katy's favourite beach is West Runton, not Cromer!");
+        alert("That's right! Katy's favourite beach is West Runton, not Cromer!");        
+    } 
 }
 
 secondQuestion()
+
+function thirdQuestion() {
+
+    let bootcamp = prompt("Is Katy completing a Full Stack Bootcamp with Tech Educators?").toLowerCase();
+
+    while (bootcamp != "yes" && bootcamp != "y" && bootcamp != "no" && bootcamp != "n") {
+        alert("Please answer with yes/no or y/n.")
+        bootcamp = prompt("Is Katy completing a Full Stack Bootcamp with Tech Educators?").toLowerCase();
+    }
+
+    if (bootcamp === "yes" || bootcamp === "y") {
+        // console.log("Correct! Katy is having great fun completing a Full Stack Bootcamp with Tech Educators!");
+        alert("Correct! Katy is having great fun completing a Full Stack Bootcamp with Tech Educators!");
+    } else if (bootcamp === "no" || bootcamp === "n") {
+        // console.log("Hmmm, that's not quite right. Katy is completeing a Full Stack Bootcamp with Tech Educators!");
+        alert("Hmmm, that's not quite right. Katy is completeing a Full Stack Bootcamp with Tech Educators!");
+    }
+}
+
+thirdQuestion()
