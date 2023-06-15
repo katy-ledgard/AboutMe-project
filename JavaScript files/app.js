c8193bb993e0b68619bbd77304b6ed64a6029f47
Katy <katy.ledgard@gmail.com>
2023-06-15 13:23:14 +0100

    add points increment to question 7

--- JavaScript files/app.js
+++ JavaScript files/app.js
@@ -1,155 +1,156 @@
 "use strict"
 
 console.log("Welcome!")
+let points = 0
 
-let user;
+// let user;
 
-alert("Hi there! Welcome to my page!")
+// alert("Hi there! Welcome to my page!")
 
-function userName() {
-    // user = console.log("Why hello there! Who might you be? Please tell me your name.")
-    user = prompt("Who might you be? Please tell me your name.")
-    alert("Hi " + user + "! My name is Katy - I hope you will enjoy getting to know about me!")
-    console.log(user)
-}
-
-userName()
-
-alert("I have a quiz for you! Please answer with yes/no or y/n.")
-
-// function firstQuestion() {
-
-    let born = prompt("Was Katy born in Norfolk?").toLowerCase();
-
-    while (born != "y" && born != "yes" && born != "n" && born != "no" /*&& born != ""*/) {
-            alert("Please answer with yes/no or y/n.");
-            born = prompt("Was Katy born in Norfolk?").toLowerCase();
-    
-}
-    if (born === "yes" || born === "y") {
-        // console.log("That's right! Katy was born in Norfolk!");
-        alert("That's right! Katy was born in Norfolk!");
-    } else if (born === "no" || born === "n") {
-        // console.log("Oops! Katy WAS born in Norfolk!")
-        alert("Oops! Katy WAS born in Norfolk!");
-    } 
-    
+// function userName() {
+//     // user = console.log("Why hello there! Who might you be? Please tell me your name.")
+//     user = prompt("Who might you be? Please tell me your name.")
+//     alert("Hi " + user + "! My name is Katy - I hope you will enjoy getting to know about me!")
+//     console.log(user)
 // }
 
-// firstQuestion()
+// userName()
 
-// function secondQuestion() {
+// alert("I have a quiz for you! Please answer with yes/no or y/n.")
 
-    let beach = prompt("Is Cromer Katy's favourite beach?").toLowerCase();
+// // function firstQuestion() {
 
-    while (beach != "yes" && beach != "y" && beach != "no" && beach != "n" /*&& beach != ""*/) {
-        alert("Please answer with yes/no or y/n.")
-        beach = prompt("Is Cromer Katy's favourite beach?").toLowerCase();
-    }
+//     let born = prompt("Was Katy born in Norfolk?").toLowerCase();
 
-    if (beach === "yes" || beach === "y") {
-        // console.log("Not quite! Katy's favourite beach is West Runton!");
-        alert("Not quite! Katy's favourite beach is West Runton!");        
-    } else if (beach === "no" || beach === "n") {
-        // console.log("That's right! Katy's favourite beach is West Runton, not Cromer!");
-        alert("That's right! Katy's favourite beach is West Runton, not Cromer!");        
-    } 
-// }
-
-// secondQuestion()
-
-// function thirdQuestion() {
-
-    let bootcamp = prompt("Is Katy completing a Full Stack Bootcamp with Tech Educators?").toLowerCase();
-
-    while (bootcamp != "yes" && bootcamp != "y" && bootcamp != "no" && bootcamp != "n" /*&& bootcamp != ""*/) {
-        alert("Please answer with yes/no or y/n.")
-        bootcamp = prompt("Is Katy completing a Full Stack Bootcamp with Tech Educators?").toLowerCase();
-    }
-
-    if (bootcamp === "yes" || bootcamp === "y") {
-        // console.log("Correct! Katy is having great fun completing a Full Stack Bootcamp with Tech Educators!");
-        alert("Correct! Katy is having great fun completing a Full Stack Bootcamp with Tech Educators!");
-    } else if (bootcamp === "no" || bootcamp === "n") {
-        // console.log("Hmmm, that's not quite right. Katy is completeing a Full Stack Bootcamp with Tech Educators!");
-        alert("Hmmm, that's not quite right. Katy is completing a Full Stack Bootcamp with Tech Educators!");
-    }
-// }
-
-// thirdQuestion()
-
-// function fourthQuestion() {
-
-    let role = prompt("Is Cosette from Les Mis Katy's dream role?").toLowerCase();
-
-    while (role != "y" && role != "yes" && role != "n" && role != "no" /*&& role != ""*/) {
-            alert("Please answer with yes/no or y/n.");
-            role = prompt("Is Cosette from Les Mis Katy's dream role?").toLowerCase();
-    
-}
-    if (role === "yes" || role === "y") {
-        // console.log("Nearly! Right musical, wrong character! Katy's dream role is Eponine!");
-        alert("Nearly! Right musical, wrong character! Katy's dream role is Eponine!");
-    } else if (role === "no" || role === "n") {
-        // console.log("You got it. Katy's dream role is actually Eponine from Les Mis!")
-        alert("You got it. Katy's dream role is actually Eponine from Les Mis!");
-    } 
+//     while (born != "y" && born != "yes" && born != "n" && born != "no" /*&& born != ""*/) {
+//             alert("Please answer with yes/no or y/n.");
+//             born = prompt("Was Katy born in Norfolk?").toLowerCase();
     
 // }
-
-// fourthQuestion()
-
-
-// function fifthQuestion() {
-
-    let dev = prompt("Does Katy want to be a Junior Developer?").toLowerCase();
-
-    while (dev != "y" && dev != "yes" && dev != "n" && dev != "no" /*&& dev != ""*/) {
-            alert("Please answer with yes/no or y/n.");
-            dev = prompt("Does Katy want to be a Junior Developer?").toLowerCase();
+//     if (born === "yes" || born === "y") {
+//         // console.log("That's right! Katy was born in Norfolk!");
+//         alert("That's right! Katy was born in Norfolk!");
+//     } else if (born === "no" || born === "n") {
+//         // console.log("Oops! Katy WAS born in Norfolk!")
+//         alert("Oops! Katy WAS born in Norfolk!");
+//     } 
     
-}
-    if (dev === "yes" || dev === "y") {
-        // console.log("Yes, that's the goal!");
-        alert("Yes, that's the goal!");
-    } else if (dev === "no" || dev === "n") {
-        // console.log("False - I DO want to become a Junior Developer!")
-        alert("False - I DO want to become a Junior Developer!");
-    } 
+// // }
+
+// // firstQuestion()
+
+// // function secondQuestion() {
+
+//     let beach = prompt("Is Cromer Katy's favourite beach?").toLowerCase();
+
+//     while (beach != "yes" && beach != "y" && beach != "no" && beach != "n" /*&& beach != ""*/) {
+//         alert("Please answer with yes/no or y/n.")
+//         beach = prompt("Is Cromer Katy's favourite beach?").toLowerCase();
+//     }
+
+//     if (beach === "yes" || beach === "y") {
+//         // console.log("Not quite! Katy's favourite beach is West Runton!");
+//         alert("Not quite! Katy's favourite beach is West Runton!");        
+//     } else if (beach === "no" || beach === "n") {
+//         // console.log("That's right! Katy's favourite beach is West Runton, not Cromer!");
+//         alert("That's right! Katy's favourite beach is West Runton, not Cromer!");        
+//     } 
+// // }
+
+// // secondQuestion()
+
+// // function thirdQuestion() {
+
+//     let bootcamp = prompt("Is Katy completing a Full Stack Bootcamp with Tech Educators?").toLowerCase();
+
+//     while (bootcamp != "yes" && bootcamp != "y" && bootcamp != "no" && bootcamp != "n" /*&& bootcamp != ""*/) {
+//         alert("Please answer with yes/no or y/n.")
+//         bootcamp = prompt("Is Katy completing a Full Stack Bootcamp with Tech Educators?").toLowerCase();
+//     }
+
+//     if (bootcamp === "yes" || bootcamp === "y") {
+//         // console.log("Correct! Katy is having great fun completing a Full Stack Bootcamp with Tech Educators!");
+//         alert("Correct! Katy is having great fun completing a Full Stack Bootcamp with Tech Educators!");
+//     } else if (bootcamp === "no" || bootcamp === "n") {
+//         // console.log("Hmmm, that's not quite right. Katy is completeing a Full Stack Bootcamp with Tech Educators!");
+//         alert("Hmmm, that's not quite right. Katy is completing a Full Stack Bootcamp with Tech Educators!");
+//     }
+// // }
+
+// // thirdQuestion()
+
+// // function fourthQuestion() {
+
+//     let role = prompt("Is Cosette from Les Mis Katy's dream role?").toLowerCase();
+
+//     while (role != "y" && role != "yes" && role != "n" && role != "no" /*&& role != ""*/) {
+//             alert("Please answer with yes/no or y/n.");
+//             role = prompt("Is Cosette from Les Mis Katy's dream role?").toLowerCase();
     
 // }
+//     if (role === "yes" || role === "y") {
+//         // console.log("Nearly! Right musical, wrong character! Katy's dream role is Eponine!");
+//         alert("Nearly! Right musical, wrong character! Katy's dream role is Eponine!");
+//     } else if (role === "no" || role === "n") {
+//         // console.log("You got it. Katy's dream role is actually Eponine from Les Mis!")
+//         alert("You got it. Katy's dream role is actually Eponine from Les Mis!");
+//     } 
+    
+// // }
 
-// fifthQuestion()
+// // fourthQuestion()
 
 
-// Add 6th Q that allows user to take numeric input to guess a number
-// alert user if their guess is too high or too low
-//give 4 chances to guess number
-//after all chances have been used, give answer
-//use a loop of some kind
+// // function fifthQuestion() {
 
-let number = 6; 
+//     let dev = prompt("Does Katy want to be a Junior Developer?").toLowerCase();
 
-console.log("testing")
+//     while (dev != "y" && dev != "yes" && dev != "n" && dev != "no" /*&& dev != ""*/) {
+//             alert("Please answer with yes/no or y/n.");
+//             dev = prompt("Does Katy want to be a Junior Developer?").toLowerCase();
+    
+// }
+//     if (dev === "yes" || dev === "y") {
+//         // console.log("Yes, that's the goal!");
+//         alert("Yes, that's the goal!");
+//     } else if (dev === "no" || dev === "n") {
+//         // console.log("False - I DO want to become a Junior Developer!")
+//         alert("False - I DO want to become a Junior Developer!");
+//     } 
+    
+// // }
 
-for (let i = 4; i > 0; i--) {
-    let guess = prompt("I am thinking of a number between 1 and 10. Can you guess what it is? You have four tries!");   
+// // fifthQuestion()
 
-if  (guess > number) {
-    alert("Oops, too high!");
-    // guess = prompt("I am thinking of a number between 1 and 10. Can you guess what it is? You have four tries!");
-} else if (guess < number) {
-    alert("Uh-oh! Too low!");
-    // guess = prompt("I am thinking of a number between 1 and 10. Can you guess what it is? You have four tries!");
-} else if (guess == number) {
-    alert("Woohoo! You guessed it!");
-    break;
-} else {
-    alert("Thats not a valid answer");
-}
-};
 
-alert("The answer was 6!")
+// // Add 6th Q that allows user to take numeric input to guess a number
+// // alert user if their guess is too high or too low
+// //give 4 chances to guess number
+// //after all chances have been used, give answer
+// //use a loop of some kind
+
+// let number = 6; 
+
+// console.log("testing")
+
+// for (let i = 4; i > 0; i--) {
+//     let guess = prompt("I am thinking of a number between 1 and 10. Can you guess what it is? You have four tries!");   
+
+// if  (guess > number) {
+//     alert("Oops, too high!");
+//     // guess = prompt("I am thinking of a number between 1 and 10. Can you guess what it is? You have four tries!");
+// } else if (guess < number) {
+//     alert("Uh-oh! Too low!");
+//     // guess = prompt("I am thinking of a number between 1 and 10. Can you guess what it is? You have four tries!");
+// } else if (guess == number) {
+//     alert("Woohoo! You guessed it!");
+//     break;
+// } else {
+//     alert("Thats not a valid answer");
+// }
+// };
+
+// alert("The answer was 6!")
 
 // add 7th question using array to have multiple options for answer
 // give 6 attempts to answer
@@ -166,18 +167,23 @@ for (let i = 0; i < 6; i++) {
 
    if (song === favSongs[0]) {
     alert("Yes!'For Good' from Wicked is such a lovely song!");
+    points++
     break;
    }  if (song === favSongs[1]) {
     alert("Absolutely! I love to try and sing all the parts of 'One Day More' from Les Misérables.");
+    points++
     break;
    } if (song === favSongs[2]) {
     alert("You got it! 'One Last Time' from Hamilton is a great song!");
+    points++
     break;
    } if (song === favSongs[3]) {
     alert("Ding ding ding! Yep, the first time I heard 'Satisfied' from Hamilton, my mind was blown!");
+    points++
     break;
    } if (song === favSongs[4]) {
     alert("True! And now the world knows that 'The World Will Know' from Newsies is a great tune!");
+    points++
     break;
    } else {
     alert("Sorry, that's not one of my top five Musical Theatre songs!");
@@ -185,9 +191,8 @@ for (let i = 0; i < 6; i++) {
 
 }
 
-// let index = favSongs.indexOf()
-
-// alert("Here are the answers you could have had... " + index);
+alert("Here are the answers you could have had... " + favSongs.map(item => item));
+alert("You have " + points + " point(s)");
 
 
 //keep track of correct answers
